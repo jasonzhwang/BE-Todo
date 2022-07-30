dbe4d321b56e8358002b0e250b515c7bfc5a0b16
jasonzhwang <0212jason@gmail.com>
2022-07-30 15:49:40 +1000

    modified:   node_modules/.cache/.eslintcache 	modified:   node_modules/.cache/default-development/0.pack 	modified:   node_modules/.cache/default-development/1.pack 	modified:   node_modules/.cache/default-development/14.pack 	modified:   node_modules/.cache/default-development/15.pack 	modified:   node_modules/.cache/default-development/19.pack 	modified:   node_modules/.cache/default-development/2.pack 	modified:   node_modules/.cache/default-development/3.pack 	modified:   node_modules/.cache/default-development/4.pack 	modified:   node_modules/.cache/default-development/5.pack 	modified:   node_modules/.cache/default-development/8.pack 	modified:   node_modules/.cache/default-development/9.pack 	modified:   node_modules/.cache/default-development/index.pack 	modified:   node_modules/.cache/default-development/index.pack.old 	new file:   src/components/layout/Footer.js 	modified:   src/index.js 	new file:   src/withgrid/App2.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import 'normalize.css';
 import './reset.css'
 import { createRoot } from 'react-dom/client';
-import App from './App';
+// import App from './App';
+import App2 from './withgrid/App2';
 import './index.css'
-// import './App.scss';
+
 
 // render(<App />, document.getElementById('root'));
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App />);
+// root.render(<App />);
+root.render(<App2 />);
